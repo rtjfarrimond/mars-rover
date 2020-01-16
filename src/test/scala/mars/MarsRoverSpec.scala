3d25a04c298924075dd8396a35f16fbb70ec5167
Robert Farrimond <rtjfarrimond@gmail.com>
2020-01-16 18:19:19 +0000

    Grid wraps
    - Add tests to ensure that the rover wraps around the grid at limits

--- src/test/scala/mars/MarsRoverSpec.scala
+++ src/test/scala/mars/MarsRoverSpec.scala
@@ -42,6 +42,42 @@ class MarsRoverSpec extends AnyFlatSpec with Matchers {
     movedRover.direction shouldBe rover.direction
   }
 
+  it should "wrap around the grid East to West" in {
+    val rover = MarsRover(Int.MaxValue, 0, East)
+    val movedRover = rover.moveForward
+
+    movedRover.x shouldBe Int.MinValue
+    movedRover.y shouldBe rover.y
+    movedRover.direction shouldBe rover.direction
+  }
+
+  it should "wrap around the grid West to East" in {
+    val rover = MarsRover(Int.MinValue, 0, West)
+    val movedRover = rover.moveForward
+
+    movedRover.x shouldBe Int.MaxValue
+    movedRover.y shouldBe rover.y
+    movedRover.direction shouldBe rover.direction
+  }
+
+  it should "wrap around the grid North to South" in {
+    val rover = MarsRover(0, Int.MaxValue, North)
+    val movedRover = rover.moveForward
+
+    movedRover.x shouldBe rover.x
+    movedRover.y shouldBe Int.MinValue
+    movedRover.direction shouldBe rover.direction
+  }
+
+  it should "wrap around the grid South to North" in {
+    val rover = MarsRover(0, Int.MinValue, South)
+    val movedRover = rover.moveForward
+
+    movedRover.x shouldBe rover.x
+    movedRover.y shouldBe Int.MaxValue
+    movedRover.direction shouldBe rover.direction
+  }
+
   "rotateClockwise" should "return a rover facing East given a rover facing North" in {
     val rover = MarsRover(0, 0, North)
     val rotatedRover = rover.rotateClockwise
@@ -96,7 +132,7 @@ class MarsRoverSpec extends AnyFlatSpec with Matchers {
     rotatedRover.direction shouldBe North
   }
 
-  it should "return a rover facing Eest given a rover facing South" in {
+  it should "return a rover facing East given a rover facing South" in {
     val rover = MarsRover(0, 0, South)
     val rotatedRover = rover.rotateAnticlockwise
 
